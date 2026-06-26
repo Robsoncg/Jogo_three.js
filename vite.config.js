855a378d5b6c4d4273a7a71057f833e79fea7323
robson <robsoncunhaag@gmail.com>
2026-06-26 09:41:36 -0300

    Corrige configuração do Vite

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,6 @@
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
-import { pages } from './src/pages.js'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
-
-const inputPages = {}
-pages.forEach(page => {
-  inputPages[page.path]= resolve(__dirname,`pages/${page.path}/index.html`);
+export default defineConfig({
+  plugins: [tailwindcss()],
 });
-
-export default   defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        ...inputPages
-       }
-    },
-  },
-  plugins:[
-    tailwindcss()
-  ]
-})
